e76266094c86d7228c89d813a72826131d23b6e2
18016204 <57203937+18016204@users.noreply.github.com>
2020-05-28 14:44:43 +0800

    done

--- app/src/main/java/com/myapplicationdev/android/demoschedulednotification/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoschedulednotification/MainActivity.java
@@ -2,13 +2,45 @@ package com.myapplicationdev.android.demoschedulednotification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+int reqCode = 12345;
+Button btnSchdeule ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSchdeule = findViewById(R.id.btnSchedule);
+
+        btnSchdeule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND ,5);
+
+                Intent intent  = new Intent(MainActivity.this,ScheduledNotificationReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,reqCode,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP,cal.getTimeInMillis(),pendingIntent);
+
+
+
+
+            }
+        });
     }
 }
